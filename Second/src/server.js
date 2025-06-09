fbb412948856bc55a53044292cfb42712699d1ad
RishabhSinghMoonCake <rishabhsinghmooncake@gmail.com>
2025-06-09 23:38:59 +0530

    Setting up PATH and making public assets static

--- Second/src/server.js
+++ Second/src/server.js
@@ -1,10 +1,24 @@
 import express from 'express'
-
+import path,{dirname} from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
 const PORT = process.env.PORT || 5000
 
+//Get the file path from the url of the current module
+
+const __filename = fileURLToPath(import.meta.url)
+//Get the directory name from the file path
+
+const __dirname = dirname(__filename)
+
+//serves the html file form the public directory
+//tells express to serve all files form the public folder as static assets
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+//serving up the HTML file from the directory
 app.get('/', (req,res)=>{
-  res.send('Hi')
+  res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
 app.listen(PORT, ()=>{
