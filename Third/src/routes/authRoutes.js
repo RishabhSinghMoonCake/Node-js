db802960393596ddbecdfb11b3bc16229da471fe
RishabhSinghMoonCake <rishabhsinghmooncake@gmail.com>
2025-06-11 18:35:43 +0530

    Completed docker incorporation

--- Third/src/routes/authRoutes.js
+++ Third/src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import db from '../db.js'
+import prisma from '../prismaClient.js'
 
 
 const router = express.Router()
 const salt = 8
 //register user enpoint /auth/register
-router.post('/register' , (req,res)=>{
+router.post('/register' , async (req,res)=>{
   const {username, password} = req.body
   
 
@@ -16,18 +16,24 @@ router.post('/register' , (req,res)=>{
 
   //save data in db
   try {
-    const insertUser = db.prepare(`INSERT INTO users (username, password)
-      VALUES (?,?)`)
-    const result = insertUser.run(username, hashedPassword)
+    const user = await prisma.user.create({
+      data:{
+        username,
+        password:hashedPassword
+      }
+    })
 
     //now that we have our user, add their first todo
     const defaultTodo = `hello :) add your first todo`
-    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
-      VALUES (? , ?)`)
-    insertTodo.run(result.lastInsertRowid, defaultTodo)
+    await prisma.todo.create({
+      data:{
+        task:defaultTodo,
+        userId:user.id
+      }
+    })
 
     //create a token
-    const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {expiresIn: '24h'})
     res.json({token})
   } catch (error) {
     console.log(error.message)
@@ -37,13 +43,15 @@ router.post('/register' , (req,res)=>{
  
 })
 
-router.post('/login', (req,res)=>{ 
+router.post('/login', async (req,res)=>{ 
   const {username, password} = req.body
 
   try{
-    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`)
-    const user = getUser.get(username)
-    console.log(user)
+    const user = await prisma.user.findUnique({
+      where: {
+        username
+      }
+    })
     if(!user)
     {
       return res.status(404).send({message:'User not Found'})
